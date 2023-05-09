4e5f1f61c7fcfda71e2aa283a047c9627ec68387
Martin Fleischmann <martin@martinfleischmann.net>
2023-05-09 13:12:01 +0200

    train on england (#34)

--- python/indicators.py
+++ python/indicators.py
@@ -28,9 +28,9 @@ class Accessibility:
         oa : pd.Series
             A series denoteing the difference in a number of jobs compared to the baseline.
             Indexed by OA code named "to_id". Series is named "oa".
-            
+
             Example::
-            
+
                 to_id
                 E00041363    38
                 E00041364    78
@@ -44,7 +44,7 @@ class Accessibility:
                 E00175604   -81
                 E00175605    51
                 Name: oa, Length: 3795, dtype: int64
-        
+
         """
         merged = xr.merge([self.baseline, xr.DataArray.from_series(oa)])
         merged["combined"] = merged.wpz_population + merged.oa.fillna(0)
@@ -56,25 +56,27 @@ class Accessibility:
         oa : pd.Series
             A series denoteing the additional square meters of parks in an OA.
             Indexed by OA code named "to_id". Series is named "oa".
-            
+
             Example::
-            
+
                 to_id
                 E00041363    7713
                 E00041364     473
                 E00041366    2092
                 E00041367    8861
                 E00041368    5830
-                             ... 
+                             ...
                 E00175601    6850
                 E00175602    9157
                 E00175603    4095
                 E00175604    2838
                 E00175605    1327
                 Name: oa, Length: 3795, dtype: int64
-        
+
         """
         merged = xr.merge([self.baseline, xr.DataArray.from_series(oa)])
         single_mode = merged.sel(mode=mode)
-        additional_acc = single_mode["oa"].where(single_mode["ttm_15"]).sum(dim=["to_id"])
+        additional_acc = (
+            single_mode["oa"].where(single_mode["ttm_15"]).sum(dim=["to_id"])
+        )
         return additional_acc + merged.sel(mode=mode)["green_accessibility"]
